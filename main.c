3d09613450e362531ff00d995dda4685dae87e60
pspiagicw <pspiagicw@gmail.com>
2025-10-27 15:52:00 +0530

    feat: added basic magic byte reading

--- main.c
+++ main.c
@@ -1,10 +1,41 @@
+#include "vm.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void error(char *message) {
-  fprintf(stdout, "%s\n", message);
+  fprintf(stderr, "%s\n", message);
   exit(1);
+}
+
+void info(char *message) { fprintf(stderr, "%s\n", message); }
+
+void confirmMagicBytes(FILE *fp) {
+  char magic[5];
+  size_t bytes_read;
+  bytes_read = fread(magic, 1, 5, fp);
+  if (bytes_read != 5) {
+    error("ERROR: Can't read magic bytes.");
+  }
+
+  printf("Magic Bytes: %s\n", magic);
+
+  if (strncmp(magic, "FENCY", 5) == 0) {
+    info("Magic Bytes confirmed!");
+  } else {
+    error("Magic bytes not matching");
+  }
+}
+
+void confirmVersion(FILE *fp) {
+  char version[1];
+  size_t bytes_read;
+  bytes_read = fread(version, 1, 1, fp);
+  if (bytes_read != 1) {
+    error("ERROR: Can't confirm version.");
+  }
+  printf("Version: %s\n", version);
 }
 
 int main(int argc, char *argv[]) {
@@ -23,8 +54,9 @@ int main(int argc, char *argv[]) {
     error("ERROR: Can't open file for reading.");
   }
 
-  int byte_read;
-  while ((byte_read = fgetc(fp)) != EOF) {
-    printf("%d ", byte_read);
-  }
+  confirmMagicBytes(fp);
+  confirmVersion(fp);
+  Constant *constants = readConstants(fp);
+  uint8_t *bytecode = readBytecode(fp);
+  run(constants, bytecode);
 }
---
+++ vm.h
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef enum {
+  INT,
+  FLOAT,
+  STRING,
+  BOOL,
+  FUNCTION,
+  CLOSURE,
+  ARRAY,
+  HASH
+} ConstantType;
+
+typedef struct {
+  ConstantType Type;
+  union {
+    bool boolean;
+    int integer;
+    double number;
+    char *string;
+  } Value;
+} Constant;
+
+void confirmMagicBytes(FILE *fp);
+void confirmVersion(FILE *fp);
+Constant *readConstants(FILE *fp);
+uint8_t *readBytecode(FILE *fp);
+int run(Constant *constants, uint8_t *bytecode);
